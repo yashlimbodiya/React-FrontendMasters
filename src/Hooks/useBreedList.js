3b949dd047d95caeeabe6f61ef2096df2c244d5a
Yashvardhan Limbodiya <yv.lim04@gmail.com>
2024-01-31 01:27:49 -0500

    useQuery + uncontrolled form

--- src/Hooks/useBreedList.js
+++ src/Hooks/useBreedList.js
@@ -1,39 +1,8 @@
-import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import fetchBreedList from "../fetchBreedList";
 
-const localCache = {};
+export default function useBreedList(animal) {
+  const results = useQuery(["breeds", animal], fetchBreedList);
 
-const useBreedList = (animal) => {
-  const [breedList, setBreedList] = useState([]);
-  const [status, setStatus] = useState("unloaded");
-
-  useEffect(() => {
-    if (!animal) {
-      setBreedList([]);
-    } else if (localCache[animal]) {
-      setBreedList[localCache[animal]];
-    } else {
-      requestBreedList();
-    }
-
-    async function requestBreedList() {
-      setBreedList([]);
-      setStatus("loading");
-
-      const res = await fetch(
-        `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
-      );
-
-      const resJson = await res.json();
-
-      localCache[animal] = resJson.breeds || [];
-
-      setStatus("loaded");
-
-      setBreedList(localCache[animal]);
-    }
-  }, [animal]);
-
-  return [breedList, status];
-};
-
-export default useBreedList;
+  return [results?.data?.breeds ?? [], results.status];
+}
